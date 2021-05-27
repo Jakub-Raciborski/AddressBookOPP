fb5976de77801b859377489029733b28b58936d1
Jakub Raciborski <raciborski.programista@gmail.com>
2021-05-27 12:07:59 +0200

    Add registerd user to file

--- KsiazkaAdresowa.cpp
+++ KsiazkaAdresowa.cpp
@@ -1,10 +1,14 @@
 #include "KsiazkaAdresowa.h"
 
+KsiazkaAdresowa::KsiazkaAdresowa(){
+    nazwaPlikuZUzytkownikami = "Uzytkownicy.txt";
+}
+
 void KsiazkaAdresowa::rejestracjaUzytkownika() {
     Uzytkownik uzytkownik = podajDaneNowegoUzytkownika();
 
     uzytkownicy.push_back(uzytkownik);
-    //dopiszUzytkownikaDoPliku(uzytkownik);
+    dopiszUzytkownikaDoPliku(uzytkownik);
 
     cout << endl << "Konto zalozono pomyslnie" << endl << endl;
     system("pause");
@@ -45,7 +49,7 @@ bool KsiazkaAdresowa::czyIstniejeLogin(string login) {
     }
     return false;
 }
-/*void KsiazkaAdresowa::dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik) {
+void KsiazkaAdresowa::dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik) {
     fstream plikTekstowy;
     string liniaZDanymiUzytkownika = "";
     plikTekstowy.open(nazwaPlikuZUzytkownikami.c_str(), ios::app);
@@ -61,16 +65,16 @@ bool KsiazkaAdresowa::czyIstniejeLogin(string login) {
     } else
         cout << "Nie udalo sie otworzyc pliku " << nazwaPlikuZUzytkownikami << " i zapisac w nim danych." << endl;
     plikTekstowy.close();
-}*/
-/*string KsiazkaAdresowa::zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik) {
+}
+string KsiazkaAdresowa::zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik) {
     string liniaZDanymiUzytkownika = "";
 
-    liniaZDanymiUzytkownika += konwerjsaIntNaString(uzytkownik.id)+ '|';
-    liniaZDanymiUzytkownika += uzytkownik.login + '|';
-    liniaZDanymiUzytkownika += uzytkownik.haslo + '|';
+    liniaZDanymiUzytkownika += konwerjsaIntNaString(uzytkownik.pobierzID())+ '|';
+    liniaZDanymiUzytkownika += uzytkownik.pobierzLogin() + '|';
+    liniaZDanymiUzytkownika += uzytkownik.pobierzHaslo() + '|';
 
     return liniaZDanymiUzytkownika;
-}*/
+}
 bool KsiazkaAdresowa::czyPlikJestPusty(fstream &plikTekstowy) {
     plikTekstowy.seekg(0, ios::end);
     if (plikTekstowy.tellg() == 0)
@@ -82,4 +86,11 @@ void KsiazkaAdresowa::wypiszWszystkichUzytkownikow() {
     const int ILOSC_UZYTKOWNIKOW = uzytkownicy.size();
     for(int i=0; i<ILOSC_UZYTKOWNIKOW; i++)
             cout<<uzytkownicy[i].pobierzLogin()<<endl;
+}
+string KsiazkaAdresowa::konwerjsaIntNaString(int liczba)
+{
+    ostringstream ss;
+    ss << liczba;
+    string str = ss.str();
+    return str;
 }
--- KsiazkaAdresowa.h
+++ KsiazkaAdresowa.h
@@ -1,5 +1,6 @@
 #include <vector>
 #include <fstream>
+#include <sstream>
 #include "Uzytkownik.h"
 
 using namespace std;
@@ -8,16 +9,19 @@ class KsiazkaAdresowa{
     int idZalogowanegoUzytkownika;
     int idOstatniegoAdresata;
     int idUsunietegoAdresata;
+    string nazwaPlikuZUzytkownikami;
     vector<Uzytkownik> uzytkownicy;
 
     Uzytkownik podajDaneNowegoUzytkownika();
     int pobierzIdNowegoUzytkownika();
     bool czyIstniejeLogin(string login);
-    //void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
-    //string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
+    void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
+    string zamienDaneUzytkownikaNaLinieZDanymiOddzielonaPionowymiKreskami(Uzytkownik uzytkownik);
     bool czyPlikJestPusty(fstream &plikTekstowy);
+    string konwerjsaIntNaString(int liczba);
 
 public:
+    KsiazkaAdresowa();
     void rejestracjaUzytkownika();
     int logowanieUzytkownika();
     void wypiszWszystkichUzytkownikow();
